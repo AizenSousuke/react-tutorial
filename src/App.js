0163af3b0ddc9732dea34d7cc3a08e421c6d2336
AizenSousuke <puteranikdimas@yahoo.com.sg>
2020-04-30 18:45:17 +0800

    Default state

--- src/App.js
+++ src/App.js
@@ -1,49 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      meaningOfLife: 42
-    }
-    console.log("constructor!");
-  }
-
-  componentDidMount() {
-    console.log("componentDidMount!");
-  }
-
-  componentDidUpdate() {
-    console.log("componentDidUpdate!");
-  }
-
-  addMeaningOfLife = () => {
-    // To use the prevState so as to guarantee the value is the latest one
-    this.setState((prevState, prevProps) => {
-      return { meaningOfLife: prevState.meaningOfLife + prevProps.increment }
-    },
-      () => console.log(this.state.meaningOfLife)
-    )
-  }
-
-  render () {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Meaning of life: { this.state.meaningOfLife }
-          </p>
-          <button className="btn" onClick={ this.addMeaningOfLife }>
-                  Add meaning of life <span className="badge badge-primary"></span>
-          </button>
-          <button className="btn" onClick={ this.toggleLifecycle }>
-                  Toggle Lifecycle <span className="badge badge-primary"></span>
-          </button>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>eCommerce</div>
+  )
 }
+
+export default App;
